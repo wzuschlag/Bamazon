f9f2fea33e57c279b013165ffdaa4fa43acbd699
wzuschlag <wzuschlag@gmail.com>
2018-02-18 18:50:05 -0600

    setup files and npm installs

---
+++ bamazonCustomer.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var prompt = require("prompt");
+var Table = require('cli-table');
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "bamazonDB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
